e10ffee200835e0d12e9c4ab1b13817501ba0781
CuongHo <cuong.holeduy@gmail.com>
2022-11-13 22:07:02 +0700

    add social sharing function

--- ImageChosen/DetailViewController.swift
+++ ImageChosen/DetailViewController.swift
@@ -16,6 +16,8 @@ class DetailViewController: UIViewController {
         
         title = selectedImaged
         navigationItem.largeTitleDisplayMode = .never
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
 
         if let imagedToLoad = selectedImaged {
             imageView.image = UIImage(named: imagedToLoad)
@@ -31,15 +33,15 @@ class DetailViewController: UIViewController {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("There is no image")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        
+        present(vc, animated: true)
     }
-    */
-
 }
